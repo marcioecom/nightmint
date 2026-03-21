87a998b828dd13c344a847b910e8aeaa58a55898
Márcio Júnior <marcio.mj70@gmail.com>
2026-03-21 19:31:01 -0300

    chore: increase anvil block-time to 4s, add indexer db script

--- packages/indexer/ponder.config.ts
+++ packages/indexer/ponder.config.ts
@@ -4,7 +4,12 @@ import { env } from "./src/env";
 
 export default createConfig({
   chains: {
-    default: {
+    anvil: {
+      id: 31337,
+      rpc: "http://127.0.0.1:8545",
+      disableCache: true,
+    },
+    sepolia: {
       id: env.PONDER_CHAIN_ID,
       rpc: env.PONDER_RPC_URL,
       pollingInterval: 2_000,
@@ -13,7 +18,7 @@ export default createConfig({
   contracts: {
     NightMintAuctionHouse: {
       abi: NightMintAuctionHouseAbi,
-      chain: "default",
+      chain: "anvil",
       address: env.PONDER_CONTRACT_ADDRESS,
       startBlock: env.PONDER_START_BLOCK,
     },
